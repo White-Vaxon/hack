a3fb102c72550ed6ae0500e6330504c249ce2aca
White-Vaxon <95180389+White-Vaxon@users.noreply.github.com>
2022-06-02 17:01:24 -0400

    Update and rename notarickroll.py to happyWheels.py

---
+++ happyWheels.py
@@ -0,0 +1,4 @@
+# Imports Webbrowser
+import webbrowser
+url =  "https://www.totaljerkface.com/happy_wheels.tjf"
+webbrowser.open_new(url)
--- notarickroll.py
+++
@@ -1,4 +0,0 @@
-#pip intstall webrowser
-import webbrowser
-url =  "https://drive.google.com"
-webbrowser.open_new(url)
